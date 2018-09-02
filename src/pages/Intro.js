12162ef0bfdecc9c69cef7c45d5c1128bb5d1622
kallemyllymaa <kalle.myllymaa@gmail.com>
2018-09-02 17:08:00 +0300

    routing added

---
+++ src/pages/Intro.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Intro.css';
+
+const Intro = () => (
+  <p className="App-intro">To get started, edit <code>src/App.js</code> and save to reload.</p>
+);
+
+export default Intro;
